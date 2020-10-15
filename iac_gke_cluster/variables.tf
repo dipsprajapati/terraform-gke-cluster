977d74b9959a326e9e7120b40a8cf8852c44caf4
dipsprajapati <dips.p.007@gmail.com>
2020-10-15 22:15:09 +0800

    Initial commit

---
+++ iac_gke_cluster/variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  default = "servian-tech-challenge"
+}
+
+variable "region" {
+  default = "asia-southeast1"
+}
+
+variable "zone" {
+  default = "asia-southeast1-a"
+}
+
+variable "cluster" {
+  default = "payever-challenge"
+}
+
+variable "credentials" {
+  default = "/Users/dipesh/mydocker/servian-tech-challenge-527f3d89ef90.json"
+}
+
+variable "kubernetes_min_ver" {
+  default = "latest"
+}
+
+variable "kubernetes_max_ver" {
+  default = "latest"
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
+
+variable "app_name" {
+  default = "payever-challenge"
+}
